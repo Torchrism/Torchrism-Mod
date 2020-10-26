5d19fa4e34913c9740fda3678f7788360e5eec49
atomuze <hdbofrank@gmail.com>
2020-10-26 18:51:15 +0800

    Revert "update ItemStaff"
    This reverts commit 14aff50ff2350f309f4738757387208eae792e3e.

--- src/main/java/com/atomuze/torchrism/item/ItemStaff.java
+++ src/main/java/com/atomuze/torchrism/item/ItemStaff.java
@@ -43,49 +43,88 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
-import net.minecraftforge.common.util.EnumHelper;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ItemStaff extends net.minecraft.item.ItemTool {
-	
 	private String name;
 	Random random = new Random();
-
-	public static final Item.ToolMaterial TORCH_STAFF_MATERIAL = EnumHelper.addToolMaterial("TORCH_STAFF", 3, 65535, 0, 0, 0);
 	private java.util.Map<String, Integer> toolClasses = new java.util.HashMap<String, Integer>();
     private static final Set<Block> EFFECTIVE_ON = Sets.newHashSet(Blocks.ACTIVATOR_RAIL, Blocks.COAL_ORE, Blocks.COBBLESTONE, Blocks.DETECTOR_RAIL, Blocks.DIAMOND_BLOCK, Blocks.DIAMOND_ORE, Blocks.DOUBLE_STONE_SLAB, Blocks.GOLDEN_RAIL, Blocks.GOLD_BLOCK, Blocks.GOLD_ORE, Blocks.ICE, Blocks.IRON_BLOCK, Blocks.IRON_ORE, Blocks.LAPIS_BLOCK, Blocks.LAPIS_ORE, Blocks.LIT_REDSTONE_ORE, Blocks.MOSSY_COBBLESTONE, Blocks.NETHERRACK, Blocks.PACKED_ICE, Blocks.RAIL, Blocks.REDSTONE_ORE, Blocks.SANDSTONE, Blocks.RED_SANDSTONE, Blocks.STONE, Blocks.STONE_SLAB, Blocks.STONE_BUTTON, Blocks.STONE_PRESSURE_PLATE);
-	NBTTagCompound nbt;
-	private int torchCount;
-    
-	public ItemStaff(String name) {
-		super(0, 0, TORCH_STAFF_MATERIAL, EFFECTIVE_ON);
+
+	public ItemStaff(ToolMaterial material, String name) {
+		super(0, 0, material, EFFECTIVE_ON);
 		setRegistryName(name);
 		setUnlocalizedName(Torchrism.MODID + "." + name);
 		setCreativeTab(Torchrism.creativeTab);
 		setMaxStackSize(1);
-		setHarvestLevel("pickaxe", 3);
 		this.name = name;
+		setHarvestLevel("pickaxe", 3);
+	}
+
+	public void registerItemModel(Item item) {
+		Torchrism.proxy.registerItemRenderer(this, 0, name);
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+		tooltip.add(I18n.format(Torchrism.MODID + "." + "torch_staff.torch_count") + this.getDamage(stack));
+	}
+
+	@Override
+	public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker) {
+		return true;
+	}
+
+	@Override
+	public boolean showDurabilityBar(ItemStack stack) {
+		return false;
 	}
 	
 	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
 		ItemStack itemstack = playerIn.getHeldItem(handIn);
 		RayTraceResult raytraceresult = this.rayTrace(worldIn, playerIn, true);
-		setNbt(itemstack);
+
+		NBTTagCompound nbt;
+		NBTTagList nbttaglist;
+	    ItemStack stack = playerIn.getHeldItem(handIn);
+	    
+		if (stack.hasTagCompound()) {
+			nbt = stack.getTagCompound();
+		} else {
+			nbt = new NBTTagCompound();
+		}
 		
-		if (raytraceresult == null) {
-			if (playerIn.isSneaking()) {
-				if (!worldIn.isRemote) {
-					for (int i = 0; i < 36; i++) {
-						if (playerIn.inventory.getStackInSlot(i).getItem().equals(new ItemStack(Blocks.TORCH).getItem())) {
-							addNbtCount("count", playerIn.inventory.getStackInSlot(i).getCount());
-							playerIn.inventory.getStackInSlot(i).shrink(playerIn.inventory.getStackInSlot(i).getCount());
-							return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, itemstack);
-						}
+		if(nbt.hasKey("count")) {
+			nbttaglist = (NBTTagList) nbt.getTag("count");
+			System.out.println("hasKey");
+		}else {
+			nbttaglist = new NBTTagList();
+		    nbt.setTag("count", nbttaglist);
+		    stack.setTagCompound(nbt);
+			nbt.setInteger("count", 1);
+			System.out.println("nhasKey");
+		}
+		
+		int a = nbt.getInteger("count");
+		nbt.setInteger("count", a + 1);
+		System.out.println(a);
+		
+		if (playerIn.isSneaking()) {
+			if (!worldIn.isRemote) {
+				for (int i = 0; i < 36; i++) {
+					if (playerIn.inventory.getStackInSlot(i).getItem().equals(new ItemStack(Blocks.TORCH).getItem())) {
+
+						playerIn.getHeldItem(handIn).damageItem(playerIn.inventory.getStackInSlot(i).getCount(),playerIn);
+						playerIn.inventory.getStackInSlot(i).shrink(playerIn.inventory.getStackInSlot(i).getCount());
 					}
 				}
 			}
+		}
+		
+		if (raytraceresult == null) {
 			return new ActionResult<ItemStack>(EnumActionResult.PASS, itemstack);
 		} else if (raytraceresult.typeOfHit != RayTraceResult.Type.BLOCK) {
 			return new ActionResult<ItemStack>(EnumActionResult.PASS, itemstack);
@@ -94,6 +133,8 @@ public class ItemStaff extends net.minecraft.item.ItemTool {
 			BlockPos blockpos = raytraceresult.getBlockPos();
 			IBlockState iblockstate = worldIn.getBlockState(blockpos);
 			Material material = iblockstate.getMaterial();
+
+			
 			
 			if (material == Material.WATER && ((Integer) iblockstate.getValue(BlockLiquid.LEVEL)).intValue() == 0 && worldIn.getBlockState(blockpos.up()).getBlock() == Blocks.AIR) {
 				worldIn.setBlockState(blockpos.up(), ModBlocks.waterTorch.getDefaultState(), 11);
@@ -101,7 +142,6 @@ public class ItemStaff extends net.minecraft.item.ItemTool {
 				playerIn.playSound(SoundEvents.BLOCK_WOOD_PLACE, 1.0F, 1.0F);
 				if (!worldIn.isRemote) {
 					playerIn.getHeldItem(handIn).damageItem(-1, playerIn);
-					addNbtCount("count", 1);
 				}
 				return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, itemstack);
 			} else {
@@ -114,98 +154,115 @@ public class ItemStaff extends net.minecraft.item.ItemTool {
 
 	@Override
 	public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
-		ItemStack itemstack = player.getHeldItem(hand);
-		setNbt(itemstack);
 		if(player.isSneaking() && worldIn.getBlockState(pos).getBlock() == Blocks.TORCH) {
 			if (!worldIn.isRemote) {
-				if(nbt.getInteger("count") > 0) {
-					addNbtCount("count", -1);
+				if(player.getHeldItem(hand).getItemDamage() + 1 < player.getHeldItem(hand).getMaxDamage()) {
+					player.getHeldItem(hand).damageItem(-1, player);
 					worldIn.spawnEntity(new EntityItem(worldIn, pos.getX() + 0.5f, pos.getY() + 0.5f, pos.getZ() + 0.5f, new ItemStack(Blocks.TORCH)));
-					return EnumActionResult.SUCCESS;
+				}else {
+					player.sendMessage(new TextComponentString(I18n.format(Torchrism.MODID + "." + "torch_staff.over_capacity")));
 				}
 			}
-			return EnumActionResult.PASS;
+			return EnumActionResult.SUCCESS;
 		}else if (worldIn.getBlockState(pos).getBlock() == ModBlocks.waterTorch || worldIn.getBlockState(pos).getBlock() == Blocks.TORCH || worldIn.getBlockState(pos).getBlock() == ModBlocks.compactedTorch || worldIn.getBlockState(pos).getBlock() == ModBlocks.doublecompactedTorch) {
 			if (!worldIn.isRemote) {
 				if (worldIn.getBlockState(pos).getBlock() == ModBlocks.waterTorch) {
-					addNbtCount("count", 1);
+					if (player.getHeldItem(hand).getItemDamage() + 1 < player.getHeldItem(hand).getMaxDamage()) {
+						player.getHeldItem(hand).damageItem(1, player);
+					} else {
+						player.sendMessage(new TextComponentString(I18n.format(Torchrism.MODID + "." + "torch_staff.over_capacity")));
+						return EnumActionResult.FAIL;
+					}
 				} else if (worldIn.getBlockState(pos).getBlock() == Blocks.TORCH) {
-					addNbtCount("count", 1);
+					if (player.getHeldItem(hand).getItemDamage() + 1 < player.getHeldItem(hand).getMaxDamage()) {
+						player.getHeldItem(hand).damageItem(1, player);
+					}else {
+						player.sendMessage(new TextComponentString(I18n.format(Torchrism.MODID + "." + "torch_staff.over_capacity")));
+						return EnumActionResult.FAIL;
+					}
 				} else if (worldIn.getBlockState(pos).getBlock() == ModBlocks.compactedTorch) {
-					addNbtCount("count", 8);
+					if (player.getHeldItem(hand).getItemDamage() + 8 < player.getHeldItem(hand).getMaxDamage()) {
+						player.getHeldItem(hand).damageItem(8, player);
+					}else {
+						player.sendMessage(new TextComponentString(I18n.format(Torchrism.MODID + "." + "torch_staff.over_capacity")));
+						return EnumActionResult.FAIL;
+					}
 				} else if (worldIn.getBlockState(pos).getBlock() == ModBlocks.doublecompactedTorch) {
-					addNbtCount("count", 64);
+					if (player.getHeldItem(hand).getItemDamage() + 64 < player.getHeldItem(hand).getMaxDamage()) {
+						player.getHeldItem(hand).damageItem(64, player);
+					}else {
+						player.sendMessage(new TextComponentString(I18n.format(Torchrism.MODID + "." + "torch_staff.over_capacity")));
+						return EnumActionResult.FAIL;
+					}
 				}
 				worldIn.setBlockState(pos, Blocks.AIR.getDefaultState());
 			}
 			ModNetworks.network.sendToDimension(new PacketModParticle(EnumParticleTypes.SMOKE_LARGE.getParticleID(), pos.getX(), pos.getY(), pos.getZ()), player.dimension);
 			return EnumActionResult.SUCCESS;
-		} else if (worldIn.getBlockState(pos).getBlock().isReplaceable(worldIn, pos) && worldIn.getBlockState(pos.down()).isNormalCube() && nbt.getInteger("count") > 0) {
+		} else if (worldIn.getBlockState(pos).getBlock().isReplaceable(worldIn, pos) && player.getHeldItem(hand).getItemDamage() > 0) {
+			
+			if (worldIn.getBlockState(pos.down()).isNormalCube()) {
 				worldIn.setBlockState(pos, Blocks.TORCH.getDefaultState());
-				addNbtCount("count", 1);
+				if (!worldIn.isRemote) {
+					player.getHeldItem(hand).damageItem(-1, player);
+				}
 				return EnumActionResult.SUCCESS;
-		} else if (worldIn.getBlockState(pos).isNormalCube() && facing != EnumFacing.DOWN && nbt.getInteger("count") > 0) {
-			addNbtCount("count", -1);
+			}
+			return EnumActionResult.FAIL;
+
+		} else if (worldIn.getBlockState(pos).isNormalCube() && player.getHeldItem(hand).getItemDamage() > 0) {
+			
+			Material m;
+			
+			if (!worldIn.isRemote) {
+				player.getHeldItem(hand).damageItem(-1, player);
+			}
 			player.playSound(SoundEvents.BLOCK_WOOD_PLACE, 1.0F, 1.0F);
-			Material m = worldIn.getBlockState(posOffset(pos ,facing)).getMaterial();
-			if((m == Material.VINE || m == Material.SNOW || m == Material.AIR) && m != Material.WATER){
-				worldIn.setBlockState(posOffset(pos ,facing), Blocks.TORCH.getDefaultState().withProperty(BlockTorch.FACING, facing));
-			}else if(m == Material.WATER) {
-				worldIn.setBlockState(posOffset(pos ,facing), ModBlocks.waterTorch.getDefaultState().withProperty(BlockTorch.FACING, facing));
+			if (facing == EnumFacing.NORTH) {
+				m = worldIn.getBlockState(pos.north()).getMaterial();
+			} else if (facing == EnumFacing.SOUTH) {
+				m = worldIn.getBlockState(pos.south()).getMaterial();
+			} else if (facing == EnumFacing.EAST) {
+				m = worldIn.getBlockState(pos.east()).getMaterial();
+			} else if (facing == EnumFacing.WEST) {
+				m = worldIn.getBlockState(pos.west()).getMaterial();
+			} else if (facing == EnumFacing.UP) {
+				m = worldIn.getBlockState(pos.up()).getMaterial();
+			} else {
+				return EnumActionResult.FAIL;
 			}
+
+			if (facing == EnumFacing.NORTH && (m == Material.VINE || m == Material.SNOW || m == Material.AIR) && m != Material.WATER) {
+				worldIn.setBlockState(pos.north(), Blocks.TORCH.getDefaultState().withProperty(BlockTorch.FACING, facing));
+			} else if (facing == EnumFacing.SOUTH && (m == Material.VINE || m == Material.SNOW || m == Material.AIR) && m != Material.WATER) {
+				worldIn.setBlockState(pos.south(), Blocks.TORCH.getDefaultState().withProperty(BlockTorch.FACING, facing));
+			} else if (facing == EnumFacing.EAST && (m == Material.VINE || m == Material.SNOW || m == Material.AIR) && m != Material.WATER) {
+				worldIn.setBlockState(pos.east(), Blocks.TORCH.getDefaultState().withProperty(BlockTorch.FACING, facing));
+			} else if (facing == EnumFacing.WEST && (m == Material.VINE || m == Material.SNOW || m == Material.AIR) && m != Material.WATER) {
+				worldIn.setBlockState(pos.west(), Blocks.TORCH.getDefaultState().withProperty(BlockTorch.FACING, facing));
+			} else if ((m == Material.VINE || m == Material.SNOW || m == Material.AIR) && m != Material.WATER) {
+				worldIn.setBlockState(pos.up(), Blocks.TORCH.getDefaultState().withProperty(BlockTorch.FACING, facing));
+			} if (facing == EnumFacing.NORTH && m == Material.WATER) {
+				worldIn.setBlockState(pos.north(), ModBlocks.waterTorch.getDefaultState().withProperty(BlockWaterTorch.FACING, facing));
+			} else if (facing == EnumFacing.SOUTH && m == Material.WATER) {
+				worldIn.setBlockState(pos.south(), ModBlocks.waterTorch.getDefaultState().withProperty(BlockWaterTorch.FACING, facing));
+			} else if (facing == EnumFacing.EAST && m == Material.WATER) {
+				worldIn.setBlockState(pos.east(), ModBlocks.waterTorch.getDefaultState().withProperty(BlockWaterTorch.FACING, facing));
+			} else if (facing == EnumFacing.WEST && m == Material.WATER) {
+				worldIn.setBlockState(pos.west(), ModBlocks.waterTorch.getDefaultState().withProperty(BlockWaterTorch.FACING, facing));
+			} else if (m == Material.WATER) {
+				worldIn.setBlockState(pos.up(), ModBlocks.waterTorch.getDefaultState().withProperty(BlockWaterTorch.FACING, facing));
+			} else {
+				return EnumActionResult.FAIL;
+			}
+
 			return EnumActionResult.SUCCESS;
-		} else if (nbt.getInteger("count") <= 0) {
-			if (worldIn.isRemote) player.sendMessage(new TextComponentString(I18n.format(Torchrism.MODID + "." + "torch_staff.no_enough_item")));		
-		}
-		return EnumActionResult.PASS;
-	}
-
-	private void addNbtCount(String NbtName, int addNum) {
-		int a = nbt.getInteger(NbtName);
-		nbt.setInteger(NbtName, a + addNum);
-	}
-	
-	private void setNbt(ItemStack itemstack) {
-		nbt = itemstack.hasTagCompound() ? itemstack.getTagCompound() : new NBTTagCompound();
-		if(!nbt.hasKey("count")) {
-		    itemstack.setTagCompound(nbt);
-			nbt.setInteger("count", 0);
-		}
-	}
-	
-	private BlockPos posOffset(BlockPos pos ,EnumFacing facing) {
-		if (facing == EnumFacing.NORTH) {
-			pos = pos.north();
-		} else if (facing == EnumFacing.SOUTH) {
-			pos = pos.south();
-		} else if (facing == EnumFacing.EAST) {
-			pos = pos.east();
-		} else if (facing == EnumFacing.WEST) {
-			pos = pos.west();
-		} else if (facing == EnumFacing.UP) {
-			pos = pos.up();
-		}
-		return pos;
-	}
-	
-	public void registerItemModel(Item item) {
-		Torchrism.proxy.registerItemRenderer(this, 0, name);
-	}
-	
-	@Override
-	@SideOnly(Side.CLIENT)
-	public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
-		NBTTagCompound nbt = stack.hasTagCompound() ? stack.getTagCompound() : new NBTTagCompound();
-		if(stack.hasTagCompound()) {
-			if(nbt.hasKey("count")) {
-				tooltip.add(I18n.format(Torchrism.MODID + "." + "torch_staff.torch_count") + nbt.getInteger("count"));
+		} else {
+			if (player.getHeldItem(hand).getItemDamage() <= 0) {
+				if (worldIn.isRemote)
+					player.sendMessage(new TextComponentString(I18n.format(Torchrism.MODID + "." + "torch_staff.no_enough_item")));
 			}
+			return EnumActionResult.FAIL;
 		}
-		
-	}
-
-	@Override
-	public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker) {
-		return true;
 	}
 }
